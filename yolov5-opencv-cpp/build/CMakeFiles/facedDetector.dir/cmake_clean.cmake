c0bf39e6f9138a7fa137ee78a0a0082ccc7d8bf2
duzongwei <894588765@qq.com>
2024-05-27 20:06:23 +0800

    yolov5 FaceDetector with opencv-c++

---
+++ yolov5-opencv-cpp/build/CMakeFiles/facedDetector.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/facedDetector.dir/yolo.cpp.o"
+  "../facedDetector.pdb"
+  "../facedDetector"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/facedDetector.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
